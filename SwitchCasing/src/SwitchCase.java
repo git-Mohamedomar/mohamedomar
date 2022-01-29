b8bc2b5b0ce0e77fc7aeae5cb81e0b0d7db9b755
moham <moham@LAPTOP-IO9U57J9>
2022-01-29 14:02:28 +0530

    21st java program

---
+++ SwitchCasing/src/SwitchCase.java
@@ -0,0 +1,20 @@
+public class SwitchCase { 
+public static void main(String args[]){ 
+ int tech = 1; 
+ 
+ switch(tech){ 
+case 1: 
+ { 
+ System.out.println("java"); 
+ break; 
+ } 
+ case 2:{ 
+ System.out.println("ES6"); 
+ break; 
+ } 
+ default:{ 
+ System.out.println("Not Listed.."); 
+ } 
+ } 
+ }
+}
